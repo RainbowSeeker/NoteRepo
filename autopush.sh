ce64d378a77fdd63b454f6d74e90d357382fea09
yangyu <1911466766@qq.com>
2024-02-29 20:27:25 +0800

    modify autopush.sh

--- autopush.sh
+++ autopush.sh
@@ -1,2 +1,32 @@
-git pull && git add . && echo "git commit -m \"`date +"%Y-%m-%d %H:%M:%S autopush"`\"" | bash  && git push origin -f
-read -s -n 1 -p "按任意键退出。。。"
+#!/bin/bash
+
+check_remote_update(){
+    git fetch 
+    git diff --quiet HEAD origin
+    if [ $? -eq 0 ]; then
+        echo "No remote updates available."
+    else
+        echo "Remote Updates are available. Ready to pull..."
+        git pull origin
+    fi
+}
+
+check_local_update(){
+    git diff --quiet HEAD .
+    if [ $? -eq 0 ]; then
+        echo "No local changes. Exiting..."
+        exit 0
+    else
+        echo "Detect local change. Ready to push..."
+    fi
+}
+
+check_remote_update
+check_local_update
+
+read -p "Please enter your commit: " commit
+if [ -z "$commit" ]; then
+    commit=`date +"%Y-%m-%d %H:%M:%S autopush"`
+fi
+git add . && echo "git commit -m \"$commit\"" | bash  && git push origin -f &&
+read -s -n 1 -p "Press any key to exit..."
