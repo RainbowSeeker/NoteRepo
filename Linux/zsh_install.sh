e3361d38ac8ee0b0e79b83c03e0e7d132933a5f1
1911466766@qq.com <1911466766@qq.com>
2024-08-03 12:34:52 +0800

    2024-08-03 12:34:51 autopush

--- Linux/zsh_install.sh
+++ Linux/zsh_install.sh
@@ -1,11 +1,7 @@
 #! /usr/bin/env bash
 
-INSTALL_SCRIPTS=install_$RANDOM.sh
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install -y zsh git curl
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh > $INSTALL_SCRIPTS
-zsh $INSTALL_SCRIPTS
+zsh <(curl -sSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh)
 echo 'skip_global_compinit=1' > ~/.zshenv 
 echo -e 'zmodule prompt-pwd\nzmodule gitster' >> ~/.zimrc
-rm -f $INSTALL_SCRIPTS
 zsh
