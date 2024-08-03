6320f0d1dbb80de3120734ae631957a95538750a
1911466766@qq.com <1911466766@qq.com>
2024-08-03 12:28:39 +0800

    2024-08-03 12:28:39 autopush

--- Linux/zsh_install.sh
+++ Linux/zsh_install.sh
@@ -6,7 +6,6 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends i
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh > $INSTALL_SCRIPTS
 zsh $INSTALL_SCRIPTS
 echo 'skip_global_compinit=1' > ~/.zshenv 
-echo -e 'zmodule prompt-pwd\nzmodule minimal' >> ~/.zimrc
-echo -e 'MNML_USER_CHAR=➜\nMNML_INSERT_CHAR=""' >> ~/.zshrc
+echo -e 'zmodule prompt-pwd\nzmodule gitster' >> ~/.zimrc
 rm -f $INSTALL_SCRIPTS
 zsh
