0bc4bd5ce79166c2f1cbdc67120a29aa94a3df08
RainbowSeeker <75429468+RainbowSeeker@users.noreply.github.com>
2025-05-08 14:31:40 +0800

    Update zsh_install.sh

--- Linux/zsh_install.sh
+++ Linux/zsh_install.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env bash
 set -e
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install -y zsh git curl exa zip 
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install -y zsh git curl zip 
 sudo chsh -s $(which zsh) $(whoami)
 zsh <(curl -sSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh)
 echo 'skip_global_compinit=1' > ~/.zshenv 
-echo -e 'zmodule prompt-pwd\nzmodule gitster\nzmodule archive\nzmodule exa\nzmodule fzf' >> ~/.zimrc
+echo -e 'zmodule prompt-pwd\nzmodule gitster\nzmodule archive\nzmodule fzf' >> ~/.zimrc
 
 # Install fzf
 curl -fsSL https://raw.githubusercontent.com/junegunn/fzf/master/install | bash -s -- --bin
