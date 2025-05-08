7a636c6be27553997a7d0e731dbbcc45100aa480
RainbowSeeker <75429468+RainbowSeeker@users.noreply.github.com>
2025-05-08 14:40:10 +0800

    Update zsh_install.sh

--- Linux/zsh_install.sh
+++ Linux/zsh_install.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 set -e
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y --quiet --no-install-recommends install -y zsh git curl zip 
-sudo chsh -s $(which zsh) $(whoami)
+chsh -s $(which zsh) $(whoami)
 zsh <(curl -sSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh)
 echo 'skip_global_compinit=1' > ~/.zshenv 
 echo -e 'zmodule prompt-pwd\nzmodule gitster\nzmodule archive\nzmodule fzf' >> ~/.zimrc
